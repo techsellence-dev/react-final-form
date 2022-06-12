776f88fa89d4425d6883d822077a538dd33e9a4d
Aparup Ganguly <aparupganguly86@gmail.com>
2022-06-12 10:10:42 +0530

    commited by aparup - validation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Field } from "react-final-form";
+import "./App.css";
+
+const required = (value) => (value ? undefined : "Required");
+const Popp = async (values) => {
+  alert("Submitted Successfully!")
+  window.alert(JSON.stringify(values, undefined, 2));
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form onSubmit={Popp}>
+          {({ handleSubmit, values, submitting }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>First Name </label>
+                <Field
+                  name="firstName"
+                  component="input"
+                  placeholder="First Name"
+                  validate={required}
+                />
+              </div>
+
+              <div>
+                <label>Last Name </label>
+                <Field
+                  name="lastName"
+                  component="input"
+                  placeholder="Last Name"
+                  validate={required}
+                />
+              </div>
+              <div>
+                <label>Email </label>
+                <Field
+                  name="email"
+                  component="input"
+                  
+                  placeholder="Email"
+                  validate={required}
+                />
+              </div>
+              <button className ="btn" type="submit">Submit</button>
+
+              <pre>{JSON.stringify(values, undefined, 2)}</pre>
+            </form>
+          )}
+        </Form>
       </header>
     </div>
   );
