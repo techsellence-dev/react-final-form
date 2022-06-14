b1ea48284c51879a0a043fe2651721ecef527490
Aparup Ganguly <aparupganguly86@gmail.com>
2022-06-14 12:20:10 +0530

    commited by Aparup - btn change and disable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Field } from "react-final-form";
 import "./App.css";
-
 const required = (value) => (value ? undefined : "Required");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const Popp = async (values) => {
-  alert("Submitted Successfully!")
+  await sleep(1000);
+  alert("Submitted Successfully!");
   window.alert(JSON.stringify(values, undefined, 2));
 };
 
+const sub = () => {
+  console.log("Submitting , Please wait...");
+};
+
 function App() {
+  const [buttonText, setButtonText] = useState("Click");
+  function handleClick() {
+    setButtonText("Submitting");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +26,7 @@ function App() {
           {({ handleSubmit, values, submitting }) => (
             <form onSubmit={handleSubmit}>
               <div>
-                <label>First Name </label>
+                <label>First Name : </label>
                 <Field
                   name="firstName"
                   component="input"
@@ -26,7 +36,7 @@ function App() {
               </div>
 
               <div>
-                <label>Last Name </label>
+                <label>Last Name :</label>
                 <Field
                   name="lastName"
                   component="input"
@@ -35,16 +45,24 @@ function App() {
                 />
               </div>
               <div>
-                <label>Email </label>
+                <label className="l"> Email Id : </label>
                 <Field
                   name="email"
                   component="input"
-                  
                   placeholder="Email"
                   validate={required}
                 />
               </div>
-              <button className ="btn" type="submit">Submit</button>
+              <button
+                className="btn"
+                type="submit"
+                disabled={submitting}
+                //  onClick ={handleClick}
+                onClick={sub}
+              >
+                {/* {buttonText} */}
+                Submit
+              </button>
 
               <pre>{JSON.stringify(values, undefined, 2)}</pre>
             </form>
