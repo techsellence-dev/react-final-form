91072eafec48eee005e41ec906ff36e1f46f7a75
KhOnsHu000007 <106870983+KhOnsHu000007@users.noreply.github.com>
2022-06-27 01:38:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './Form';
 import SignIn from './SignIn'
 import ForgotPassword from './ForgotPassword'
 import Confirm from './Confirm'
