069b64a8f5d6a49617c19305d1db927bb5d25754
KhOnsHu000007 <106870983+KhOnsHu000007@users.noreply.github.com>
2022-07-09 22:37:13 +0530

    Add files via upload

---
+++ src/RequireAuth.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { useAuth } from './Protected2'
+
+export const RequireAuth = ({children}) => {
+    const auth=useAuth()
+
+    if(!auth.user){
+        return<Navigate to='/'/>
+    }
+
+    return children
+  
+}
