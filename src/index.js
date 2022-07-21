22364e146bd547861b2f1dc73e0c10f558be20d9
gazalmighty <98213852+techsellence-dev@users.noreply.github.com>
2022-07-21 18:26:07 +0530

    signup changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import SignIn from './SignIn'
-import ForgotPassword from './ForgotPassword'
-import Confirm from './Confirm'
-import SignUp from './SignUp';
-import ConfirmSignUp from './ConfirmSignUp';
-import { AuthProvider } from './Protected2'
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import SignIn from "./SignIn";
+import ForgotPassword from "./ForgotPassword";
+import Confirm from "./Confirm";
+import SignUp from "./SignUp";
+import ConfirmSignUp from "./ConfirmSignUp";
+import { AuthProvider } from "./Protected2";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <AuthProvider>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
     </AuthProvider>
   </BrowserRouter>
-
 );
